4085329ea3185ccda3eb876fe035412e3f02f5e2
ali0iraq <30835872+ali0iraq@users.noreply.github.com>
2018-06-26 17:19:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -87,23 +87,6 @@ client.on('message', message => {
  
 
 
-uesr.on('message', function(message) {
-  if (message.channel.type === "dm") {
-      if (message.author.id === uesr.user.id) return;
-      var iiMo = new Discord.RichEmbed()
-      .setColor('RANDOM')
-      .setTimestamp()
-      .setTitle('``هذة الشخص قام بالكتابة !``')
-      .setThumbnail(`${message.author.avatarURL}`)
-      .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
-      .setFooter(`From **${message.author.tag} (${message.author.id})**`)
-  uesr.channels.get("461128496790634506").send({embed:iiMo});
-  }
-});
-
-uesr.login ("NDU3MzI0ODY2MTgyNDQ3MTI2.Dg_ANg.wKr6my58fxNdg0ljWJz3H53DDKQ")
-
-
  bot.on('message', function(message) {
     if (message.channel.type === "dm") {
         if (message.author.id === bot.user.id) return;
